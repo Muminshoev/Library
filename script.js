0202d55daf47db721380a22751162da3e19ad31b
Hiloli <mhiloli17@gmail.com>
2025-07-16 15:22:52 +0500

    added style, made script.js work

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
-let myLibrary = [];
+const myLibrary = [];
 
 
-
-
-
-let addNewBook = (ev) => {
-    ev.preventDefault();
-    let books  = {
-        title: document.getElementById('title').value,
-        author: document.getElementById('author').value,
-        pages: document.getElementById('pages').value,
-    };
-    myLibrary.push(books);
-    document.querySelector('.form').reset();
-    localStorage.setItem('MyLibrary', JSON.stringify(myLibrary) );
-    
-
+function Book(title, author, pages, finished) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.finished = finished;
 };
 
-document.addEventListener('DOMContentLoaded', () =>{
-    document.getElementById('btn').addEventListener('click', addNewBook)
-});
+
+function addBookToLibrary() {
+    const bookTitle = document.getElementById('title').value;
+    const bookAuthor = document.getElementById('author').value;
+    const bookPages = document.getElementById('pages').value;
+    const didRead = document.getElementById('read').checked;
+    const form = document.getElementById('form');
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const book1 = new Book (bookTitle, bookAuthor, bookPages, didRead);
+
+        myLibrary.push(book1);
+        console.log(myLibrary);
+        form.reset()
+    })
+
+    
+};
+
+addBookToLibrary();
